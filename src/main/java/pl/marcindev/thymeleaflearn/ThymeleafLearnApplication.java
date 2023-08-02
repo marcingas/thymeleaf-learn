e7c43f81fd7e92b719e2b4eaf614bce1160d0672
Marcin <gasiorek.mail@gmail.com>
2023-08-02 06:26:26 +0200

    Initial commit

---
+++ src/main/java/pl/marcindev/thymeleaflearn/ThymeleafLearnApplication.java
@@ -0,0 +1,13 @@
+package pl.marcindev.thymeleaflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcindev/thymeleaflearn/ThymeleafLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcindev.thymeleaflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
