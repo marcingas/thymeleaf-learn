215916d09ea77d3f6f682664b27e5844272c2934
Marcin <gasiorek.mail@gmail.com>
2023-08-03 07:05:05 +0200

    min max regex custom error message etc.

--- src/main/java/pl/marcindev/thymeleaflearn/model/Customer.java
+++ src/main/java/pl/marcindev/thymeleaflearn/model/Customer.java
@@ -1,14 +1,22 @@
 package pl.marcindev.thymeleaflearn.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class Customer {
     private String firstName;
+
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "greater then 0")
+    @Max(value = 10, message = "must be less then 11")
+    private Integer freePasses;
+
+    @Pattern(regexp = "[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
 }
