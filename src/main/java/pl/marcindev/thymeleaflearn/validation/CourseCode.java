e79fafd622c6cb4a68a62bf3c5dec170f8500a18
Marcin <gasiorek.mail@gmail.com>
2023-08-03 07:17:18 +0200

    created custom Annotation

---
+++ src/main/java/pl/marcindev/thymeleaflearn/validation/CourseCode.java
@@ -0,0 +1,25 @@
+package pl.marcindev.thymeleaflearn.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+    public String value() default "DEV";
+
+    public String message() default "must start with DEV";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+
+
+}
