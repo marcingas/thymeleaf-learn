e5593943727f1cce5f639099df3c4602bd1e17ed
Marcin <gasiorek.mail@gmail.com>
2023-08-03 06:24:07 +0200

    added initBinder to trip extra white spaces

--- src/main/java/pl/marcindev/thymeleaflearn/controller/CustomerController.java
+++ src/main/java/pl/marcindev/thymeleaflearn/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package pl.marcindev.thymeleaflearn.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.marcindev.thymeleaflearn.model.Customer;
@@ -13,6 +16,11 @@ import java.awt.*;
 
 @Controller
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @GetMapping("/")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
