7810530e7a708f81b77847db36a5400365a4ef9c
Marcin <gasiorek.mail@gmail.com>
2023-08-03 06:16:07 +0200

    implemented customer form and customer confirmation

--- src/main/java/pl/marcindev/thymeleaflearn/controller/CustomerController.java
+++ src/main/java/pl/marcindev/thymeleaflearn/controller/CustomerController.java
@@ -1,8 +1,12 @@
 package pl.marcindev.thymeleaflearn.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.marcindev.thymeleaflearn.model.Customer;
 
 import java.awt.*;
@@ -10,8 +14,16 @@ import java.awt.*;
 @Controller
 public class CustomerController {
     @GetMapping("/")
-    public String showForm(Model model){
-        model.addAttribute("customer",new Customer());
+    public String showForm(Model model) {
+        model.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer customer,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "customer-form";
+        else return "customer-confirmation";
     }
 }
